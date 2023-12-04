140ed72526cf44320e07a40348c9a498a3e9dedd
rakshit <rakshit.2002.rc@gmail.com>
2023-12-05 03:28:44 +0530

    lms started 129 with navbar and basic outline is functioning well

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+const user = () => {
+  return (
+    <div >
+        {/* <UserButton afterSignOutUrl="/"/> */}
+    </div>
+  )
+}
+
+export default user
