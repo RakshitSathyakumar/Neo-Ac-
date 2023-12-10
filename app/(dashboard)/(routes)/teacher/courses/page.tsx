50f2f4720625b10e19dbd6783e2cc240becb7329
rakshit <rakshit.2002.rc@gmail.com>
2023-12-11 00:46:37 +0530

    lms 145 done

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const Courses = () => {
   return (
-    <div>Courses</div>
+    <div>
+      <Link href={'/teacher/create'}>
+      <Button variant={'destructive'}>New Course</Button>
+      </Link>
+    </div>
   )
 }
 
